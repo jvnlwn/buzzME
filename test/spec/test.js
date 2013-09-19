98e8e2a05af6f6563d1a825eedaf751120c07948
FlackAP <flack.ap@gmail.com>
2013-09-19 16:58:48 -0400

    began writing test for fetch js

--- test/spec/test.js
+++ test/spec/test.js
@@ -26,4 +26,20 @@
 			},3000)
 		})
 	});
+
+	describe('should fetch new data every three seconds', function(done) {
+		var message = new MessageClass();
+		var messageContent = 'Greatest message ever # ' + Math.floor(Math.random()*100000);
+
+		$('#usermsg').val(messageContent)
+		$('#submitmsg').click();
+
+		setTimeout(function() {
+			setTimeout(function(){
+				
+			}, 5000);
+		}, 2000)
+
+	})
+
 })();
